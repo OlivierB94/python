af8d4ac42077af84233aada3872a27a93030e0b5
OlivierB94 <spt.olivier.94+mooc@gmail.com>
2017-09-19 23:26:05 +0200

    creation 1iere version Exemple-1.py

---
+++ BeginningWithPython/Exemple-1.py
@@ -0,0 +1,13 @@
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
+
+def show_random_item(my_list):
+    # get a random number
+    item = my_list[0] # get a quote from a list
+    return item # return the item
+
+user_answer = "A"
+
+while user_answer != "B":
+    print(show_random_item(quotes))
