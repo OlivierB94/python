0245311c7cb1455ab3a7519b921e0b2f04f38df8
OlivierB94 <spt.olivier.94+mooc@gmail.com>
2017-09-19 23:29:35 +0200

    2nd version Exemple-1.py. sortie de boucle while

--- BeginningWithPython/Exemple-1.py
+++ BeginningWithPython/Exemple-1.py
@@ -1,5 +1,4 @@
-quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
-
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."] 
 characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
 def show_random_item(my_list):
@@ -11,3 +10,4 @@ user_answer = "A"
 
 while user_answer != "B":
     print(show_random_item(quotes))
+    user_answer = "B"
